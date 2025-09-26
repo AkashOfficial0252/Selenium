7f70a9ba272449d88ed2541866564bf00b2f3499
Akash tiwari <akashji0252@gmail.com>
2025-09-26 16:30:51 +0530

    commit

--- cchange.java
+++ cchange.java
@@ -2,4 +2,9 @@ package Selenium;
 
 public class cchange {
 
+	public static void main(String[] args) {
+		  System.out.println("Opened Chrome with profile and navigated to: ");
+		        System.out.println("Opened Chrome with profile and navigated to: ");
+	}
+
 }
